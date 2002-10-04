6ef41690fafc7d9b41c92c45599ea4985b2c5eb2
Simon Peter <dn.tlp@gmx.net>
2002-10-04 06:18:08 -0700

    AdPlay 1.2 updates.

--- adplay.cc
+++ adplay.cc
@@ -1,23 +1,21 @@
-/* 
-   adplay - OPL2 audio player
-
-   Copyright (C) 2001, 2002 Simon Peter
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
-
-*/
+/*
+ * AdPlay/UNIX - OPL2 audio player
+ * Copyright (C) 2001, 2002 Simon Peter <dn.tlp@gmx.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+ */
 
 #include <iostream>
 #include <stdlib.h>
@@ -29,13 +27,11 @@
 #include <sys/soundcard.h>
 #include <adplug/adplug.h>
 #include <adplug/emuopl.h>
-#include "system.h"
+
 #include "getopt.h"
-#include "config.h"
 
 // defines
-#define EXIT_SUCCESS	0
-#define EXIT_FAILURE	1
+#define ADPLAY_VERSION	"AdPlay/UNIX " VERSION	// AdPlay/UNIX version string
 
 // defaults
 #define BUF_SIZE	512		// buffer size in samples
@@ -49,13 +45,12 @@
 #define SONGMESSAGE	false		// show song message
 #define SUBSONG		0		// default subsong
 
-CAdPlug	adplug;				// global AdPlug object
-CPlayer *p;				// global player object
-CEmuopl	*opl;				// global emulator object
-int	audio_fd;			// audio device file
-char	*program_name;			// program executable name
+// global variables
+CEmuopl *opl;			// global emulator object
+int audio_fd;			// audio device file
+const char *program_name;	// Program executable name
 
-// Option variables
+// option variables
 int		buf_size=BUF_SIZE,freq=FREQ,channels=CHANNELS,bits=BITS;
 unsigned int	subsong=SUBSONG;
 char		*device=DEVICE;
@@ -67,12 +62,14 @@ bool		endless=ENDLESS,showinsts=SHOWINSTS,songinfo=SONGINFO,songmessage=SONGMESS
 
 #define getsampsize (channels * (bits / 8))
 
-static void usage (int status)
+static void usage (int exit_status)
+/*
+ * Print usage information and exit with exit_status.
+ */
 {
-  printf (_("%s - OPL2 audio player\n"), program_name);
-  printf (_("Usage: %s [OPTION]... [FILE]...\n"), program_name);
-  printf (_("\
-Options:
+	puts(ADPLAY_VERSION " - OPL2 audio player");
+	printf("Usage: %s [OPTION]... [FILE]...\n",program_name);
+	printf("Options:\n\
   -8, --8bit                 8-bit replay\n\
       --16bit                16-bit replay\n\
   -f, --freq=FREQ            set frequency to FREQ\n\
@@ -86,9 +83,9 @@ Options:
   -s, --subsong=N            play subsong number N\n\
   -o, --once                 play only once, don't loop\n\
   -h, --help                 display this help and exit\n\
-  -V, --version              output version information and exit\n\
-"));
-  exit(status);
+  -V, --version              output version information and exit\n");
+
+	exit(exit_status);
 }
 
 static int decode_switches(int argc, char **argv)
@@ -97,6 +94,7 @@ static int decode_switches(int argc, char **argv)
  * Return the index of the first non-option argument.
  */
 {
+  int c;
   struct option const long_options[] = {
     {"8bit",no_argument,NULL,'8'},		// 8-bit replay
     {"16bit",no_argument,NULL,'1'},		// 16-bit replay
@@ -114,53 +112,24 @@ static int decode_switches(int argc, char **argv)
     {"version", no_argument, 0, 'V'},		// version information
     {NULL, 0, NULL, 0}				// end of options
   };
-  int c;
 
   while ((c = getopt_long(argc, argv, "8f:b:d:irms:ohV", long_options, (int *) 0)) != EOF) {
       switch (c) {
-      case '8':
-	bits = 8;
-	break;
-      case '1':
-	bits = 16;
-	break;
-      case 'f':
-	freq = atoi(optarg);
-	break;
-      case '3':
-	channels = 2;
-	break;
-      case '2':
-	channels = 1;
-	break;
-      case 'b':
-	buf_size = atoi(optarg);
-	break;
-      case 'd':
-	device = optarg;
-	break;
-      case 'i':
-	showinsts = true;
-	break;
-      case 'r':
-	songinfo = true;
-	break;
-      case 'm':
-	songmessage = true;
-	break;
-      case 's':
-	subsong = atoi(optarg);
-	break;
-      case 'o':
-	endless = false;
-	break;
-      case 'V':
-	printf("adplay %s\n", VERSION);
-	exit(EXIT_SUCCESS);
-      case 'h':
-	usage(EXIT_SUCCESS);
-      default:
-	usage(EXIT_FAILURE);
+      case '8': bits = 8; break;
+      case '1': bits = 16; break;
+      case 'f': freq = atoi(optarg); break;
+      case '3': channels = 2; break;
+      case '2': channels = 1; break;
+      case 'b': buf_size = atoi(optarg); break;
+      case 'd': device = optarg; break;
+      case 'i': showinsts = true; break;
+      case 'r': songinfo = true; break;
+      case 'm': songmessage = true; break;
+      case 's': subsong = atoi(optarg); break;
+      case 'o': endless = false; break;
+      case 'V': puts(ADPLAY_VERSION); exit(0);
+      case 'h':	usage(0);
+      default: usage(1);
       }
   }
 
@@ -169,14 +138,16 @@ static int decode_switches(int argc, char **argv)
 
 void play(char *fn)
 {
-	char	*pos,*audiobuf;
-	long	i,towrite,minicnt=0;
-	bool	playing=true;
+	CPlayer *p = CAdPlug::factory(fn,opl);
+	char *pos,*audiobuf;
+	long i,towrite,minicnt=0;
+	bool playing=true;
 
-	if(!(p = adplug.factory(fn, opl))) {
+	if(!p) {
 		cout << program_name << ": " << fn << ": Unknown filetype" << endl;
 		return;
 	}
+
 	p->rewind(subsong);
 
 	cout << "Playing '" << fn << "'..." << endl <<
@@ -184,20 +155,19 @@ void play(char *fn)
 		"Title : " << p->gettitle() << endl <<
 		"Author: " << p->getauthor() << endl << endl;
 
-	// display instruments
-	if(showinsts) {
+	if(showinsts) {		// display instruments
 		cout << "Instrument names:" << endl;
 		for(i=0;i<(long)p->getinstruments();i++)
 			cout << i << ": " << p->getinstrument(i) << endl;
 		cout << endl;
 	}
 
-	// display song message
-	if(songmessage) {
+	if(songmessage) {	// display song message
 		cout << "Song message:" << endl;
 		cout << p->getdesc() << endl << endl;
 	}
 
+	// play loop
 	audiobuf = new char [buf_size * getsampsize];
 	while(playing || endless) {
 		if(songinfo) {	// display song info
@@ -220,6 +190,7 @@ void play(char *fn)
 		}
 		write(audio_fd, audiobuf, buf_size*getsampsize);
 	}
+
 	delete [] audiobuf;
 	delete p;
 }
@@ -231,14 +202,10 @@ int main(int argc, char **argv)
   // parse commandline
   program_name = argv[0];
   optind = decode_switches(argc,argv);
+  if(optind == argc) usage(1);			// no filename given
+  if(argc - optind > 1) endless = false;	// more than 1 file given
 
-  if(optind == argc)
-    usage(EXIT_FAILURE);
-
-  if(argc - optind > 1)
-    endless = false;
-
-  // open audio device
+  // open OSS audio device
   if((audio_fd = open(device, O_WRONLY, 0)) == -1) {
     perror(device);
     exit(EXIT_FAILURE);
@@ -250,7 +217,7 @@ int main(int argc, char **argv)
 
   // main loop
   opl = new CEmuopl(freq,bits == 16,channels == 2);
-  for(i=optind;i<argc;i++) {
+  for(i=optind;i<argc;i++) {	// play all files from commandline
     opl->init();
     play(argv[i]);
   }
@@ -258,5 +225,5 @@ int main(int argc, char **argv)
   // deinit
   delete opl;
   close(audio_fd);
-  exit(EXIT_SUCCESS);
+  exit(0);
 }
